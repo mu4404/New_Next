c277bb22ceb0ea86d38db74a900cb734162e6073
뮤냥 <ckdals4693@gmail.com>
2025-05-15 15:40:01 +0900

    로그인 및 회원가입 페이지 완료

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,37 +1,42 @@
 "use client";
 
 import { useState } from "react";
-import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-export default function LoginPage() {
+export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
 
-  const handleLogin = async () => {
+  const handleRegister = async () => {
     if (!email || !password) {
-      alert("이메일과 비밀번호를 입력하세요.");
+      alert("이메일과 비밀번호를 입력하세요");
       return;
     }
-    const res = await signIn("credentials", {
-      email,
-      password,
-      redirect: false,
+
+    const res = await fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
     });
-    if (res?.ok) {
-      router.push("/");
+
+    if (res.ok) {
+      alert("회원가입 성공! 로그인 페이지로 이동합니다.");
+      router.push("/login");
     } else {
-      alert("로그인 실패: 이메일 또는 비밀번호를 확인하세요");
+      const data = await res.json();
+      alert(`회원가입 실해: ${data.message}`);
     }
   };
+
   return (
     <div className="max-w-md mx-auto mt-20">
-      <h2 className="text-2x1 font-bold mb-6 text-center">로그인</h2>
+      <h2 className="text-2xl font-bold mb-6 text-center">회원가입</h2>
       <input
         type="email"
         placeholder="이메일"
         className="w-full p-2 mb-3 border rounded"
+        value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
@@ -42,10 +47,10 @@ export default function LoginPage() {
         onChange={(e) => setPassword(e.target.value)}
       />
       <button
-        className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
-        onClick={handleLogin}
+        className="w-full bg-green-600 text-white py-2 rounded hover:bg-green-700"
+        onClick={handleRegister}
       >
-        로그인
+        회원가입
       </button>
     </div>
   );
